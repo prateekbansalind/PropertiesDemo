e0dbaa34b9699c51023d6fdb52df37c22e43eacb
pbansal16 <prateekbansalind@gmail.com>
2021-10-30 18:10:33 +1300

    Add project files.

---
+++ Properties/PersonModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class PersonModel
+    {
+        public string FirstName { private get; set; }
+        public string LastName { get; private set; }
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get 
+            {
+                string output;
+
+                output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+
+                _ssn = output;
+
+                return _ssn; 
+            }
+            set { _ssn = value; }
+        }
+
+        public PersonModel(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public PersonModel()
+        {
+
+        }
+
+        private string _fullname;
+
+        public string FullName
+        {
+            get
+            {
+                _fullname = FirstName + " " + LastName;
+                return _fullname; 
+            }
+            
+        }
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel("Bansal");
+            person.FirstName = "Prateek";
+            
+            person.SSN = "161-67-0614";
+
+            Console.WriteLine($"{person.FullName} : {person.SSN}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
